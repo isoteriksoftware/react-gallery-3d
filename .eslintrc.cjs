d11c56e8f94394fc60926c566bc8934eff034dc6
imran <isoteriksoftware@gmail.com>
2023-11-09 13:43:40 +0100

    installed husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,11 +8,34 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            {allowConstantExport: true},
-        ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "tsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
+    "plugins": ["react", "@typescript-eslint", "react-hooks", "prettier","simple-import-sort"],
+    "rules": {
+        "camelcase": "error",
+        "no-duplicate-imports": "error",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-explicit-any":"off",
+        "react/react-in-jsx-scope":"off",
+        "no-console": "error",
+        "no-alert": "error",
+        "react-hooks/exhaustive-deps": "off",
+        "react/prop-types": 0,
+        "react/display-name": 0,
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "@typescript-eslint/no-empty-function":"off",
+        "react/no-unknown-property":"off",
+        "react/no-unescaped-entities ":"off"
+    },
+    "settings": {
+        "import/resolver": {
+            "typescript": {}
+        }
+    }
 }
