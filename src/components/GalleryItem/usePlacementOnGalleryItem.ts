914c619588bbc985e07db0cbb05bf0851188e360
imran <isoteriksoftware@gmail.com>
2023-11-14 21:54:12 +0100

    fixed placement calculation

--- src/components/GalleryItem/usePlacementOnGalleryItem.ts
+++ src/components/GalleryItem/usePlacementOnGalleryItem.ts
@@ -3,8 +3,13 @@ import { useEffect, useMemo } from "react";
 import { Vector3 } from "three";
 import { calculatePlacementOnGalleryItem } from "react-gallery-3d";
 
-const usePlacementOnGalleryItem = (objectOffset: number = 0) => {
-  const { itemCount, item } = useGallery();
+/**
+ * Calculates the position and orientation vectors for an object to be placed on a gallery item (cylinder segment).
+ * @param objectOffset - The offset distance from the item's surface to the object's center.
+ * @param itemAlignmentOffset - An optional angular offset to adjust the alignment
+ */
+const usePlacementOnGalleryItem = (objectOffset: number = 0, itemAlignmentOffset?: number) => {
+  const { item, itemCount } = useGallery();
   const { sectionAngle, outerRadius, itemIndex } = item;
 
   if (!itemIndex && itemIndex != 0) {
@@ -20,11 +25,21 @@ const usePlacementOnGalleryItem = (objectOffset: number = 0) => {
       outerRadius,
       sectionAngle,
       itemCount,
+      itemAlignmentOffset,
       objectOffset,
       position,
       orientation,
     );
-  }, [sectionAngle, outerRadius, itemIndex, position, orientation, objectOffset, itemCount]);
+  }, [
+    sectionAngle,
+    outerRadius,
+    itemIndex,
+    position,
+    orientation,
+    objectOffset,
+    itemAlignmentOffset,
+    itemCount,
+  ]);
 
   return { position, orientation };
 };
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -7,7 +7,9 @@ import { Vector3 } from "three";
  * @param {number} itemIndex - The index of the item in the gallery.
  * @param {number} outerRadius - The outer radius of the item.
  * @param {number} sectionAngle - The angular size of the item in radians.
- * @param itemCount - The total number of items(segments) in the gallery.
+ * @param {number} itemCount - The total number of items in the gallery.
+ * @param {number} itemAlignmentOffset - An optional angular offset to adjust the alignment
+ *  *                                            of the object within the item. This value is calculated if not provided.
  * @param {number} [objectOffset=0] - The offset distance from the item's surface to the object's center. Defaults to 0.
  * @param {Vector3} [position] - Optional vector to be filled with the calculated position.
  * @param {Vector3} [orientation] - Optional vector to be filled with the calculated orientation.
@@ -18,26 +20,36 @@ export const calculatePlacementOnGalleryItem = (
   outerRadius: number,
   sectionAngle: number,
   itemCount: number,
+  itemAlignmentOffset?: number,
   objectOffset: number = 0,
   position: Vector3 = new Vector3(),
   orientation: Vector3 = new Vector3(),
 ): { position: Vector3; orientation: Vector3 } => {
-  // Calculate the center angle for the segment, taking into account the gallery's global rotation offset if necessary
-  // If the gallery starts segmenting from an angle other than the mathematically assumed zero angle,
-  // you may need to determine and add a globalRotationOffset here.
-  const globalRotationOffset = sectionAngle / outerRadius / 2;
-  const centerAngle =
-    ((itemCount - itemIndex) * sectionAngle + sectionAngle / 2 - globalRotationOffset) %
-    (2 * Math.PI);
+  const alignmentOffset = itemAlignmentOffset ?? calculateItemAlignmentOffset(itemCount);
+  const angularOffset = sectionAngle / 2 + alignmentOffset;
 
-  // Convert polar coordinates to Cartesian coordinates for the position
+  const centerAngle = ((itemCount - itemIndex) * sectionAngle + angularOffset) % (2 * Math.PI);
+
   position.set(outerRadius * Math.cos(centerAngle), 0, outerRadius * Math.sin(centerAngle));
 
-  // The orientation vector should point inwards, towards the center of the gallery
   orientation.set(Math.cos(centerAngle), 0, Math.sin(centerAngle)).normalize();
 
-  // Adjust the position by the objectOffset along the orientation vector
   position.addScaledVector(orientation, objectOffset);
 
   return { position, orientation };
+};
+
+/**
+ * Calculates the angular offset to align an object within a gallery item (segment) using a quadratic polynomial.
+ * The polynomial used is y = -0.0407x^2 + 0.6961x - 2.1934, which was derived from a set of data points to approximate
+ * their pattern.
+ *
+ * @param {number} itemCount - The total number of items in the gallery.
+ */
+export const calculateItemAlignmentOffset = (itemCount: number) => {
+  const a = -0.04071429;
+  const b = 0.69614286;
+  const c = -2.19342857;
+
+  return a * itemCount * itemCount + b * itemCount + c;
 };
