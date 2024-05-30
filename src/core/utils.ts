7a3e4a6dde6926adecc2640ad6e8872464c4a198
imran <isoteriksoftware@gmail.com>
2024-05-30 09:06:54 +0100

    refactored utils

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -8,9 +8,9 @@ import { Vector3 } from "three";
  * @param {number} outerRadius - The outer radius of the item.
  * @param {number} sectionAngle - The angular size of the item in radians.
  * @param {number} itemCount - The total number of items in the gallery.
- * @param {number} itemAlignmentOffset - An optional angular offset to adjust the alignment
- *  *                                            of the object within the item. This value is calculated if not provided.
- * @param {number} [objectOffset=0] - The offset distance from the item's surface to the object's center. Defaults to 0.
+ * @param {number} [itemAlignmentOffset] - An optional angular offset to adjust the alignment
+ *                                             of the object within the item. This value is calculated if not provided.
+ * @param {number} [objectOffset] - The offset distance from the item's surface to the object's center. Defaults to 0.
  * @param {Vector3} [position] - Optional vector to be filled with the calculated position.
  * @param {Vector3} [orientation] - Optional vector to be filled with the calculated orientation.
  * @returns {{ position: Vector3, orientation: Vector3 }} - The position and orientation vectors.
@@ -46,21 +46,20 @@ export const calculatePlacementOnGalleryItem = (
 /**
  * Calculates the angular offset to align an object within a gallery item (segment) using a polynomial of degree 5.
  * The polynomial coefficients were determined through interpolation
- * of the points (3, -0.5), (4, 0), (5, 0.3), (6, 0.52), (7, 0.66), and (8, 0.78). This function is guaranteed to
+ * of the points (3, -0.5), (4, 0), (5, 0.3), (6, 0.52), (7, 0.66), and (8, 0.78).
+ *
+ * This function is guaranteed to
  * return a value between -0.5 and 0.78 for any item count between 3 and 8.
- * It returns the angular offset for a given item count.
- * Compared to {@link calculateItemAlignmentOffsetQuadratic}, this is more accurate but also more expensive, and
+ *
+ * Compared to {@link calculateItemAlignmentOffsetQuadratic}, this is more accurate but also less efficient, and
  * it doesn't yield good results for item counts outside the range of 3 and 8.
  *
  * @param {number} itemCount - The total number of items in the gallery.
+ * @returns {number} - The angular offset for the given item count.
  */
 export const calculateItemAlignmentOffsetPolynomial = (itemCount: number): number => {
   const coefficients = [0.0015, -0.0425, 0.4775, -2.6975, 7.981, -9.98];
-  const y = coefficients.reduce(
-    (acc, curr, i) => acc + curr * Math.pow(itemCount, coefficients.length - 1 - i),
-    0,
-  );
-  return parseFloat(y.toFixed(2));
+  return calculateAngularOffset(coefficients, itemCount);
 };
 
 /**
@@ -69,16 +68,21 @@ export const calculateItemAlignmentOffsetPolynomial = (itemCount: number): numbe
  * to the points (3, -0.5), (4, 0), (5, 0.3), (6, 0.52), (7, 0.66), and (8, 0.78).
  * It returns the angular offset for a given item count.
  *
- * Compared to {@link calculateItemAlignmentOffsetPolynomial}, this is less accurate but also less expensive,
+ * Compared to {@link calculateItemAlignmentOffsetPolynomial}, this is less accurate but also more efficient,
  * and it yields close enough results for item counts outside the range of 3 and 8.
  *
- * @param {number} x - The x value for which the y value needs to be calculated.
+ * @param {number} itemCount - The total number of items in the gallery.
  */
-export const calculateItemAlignmentOffsetQuadratic = (x: number): number => {
+export const calculateItemAlignmentOffsetQuadratic = (itemCount: number): number => {
   const coefficients = [-0.04535714, 0.74464286, -2.29785714];
-  const y = coefficients.reduce(
-    (acc, curr, i) => acc + curr * Math.pow(x, coefficients.length - 1 - i),
+  return calculateAngularOffset(coefficients, itemCount);
+};
+
+const calculateAngularOffset = (coefficients: number[], itemCount: number) => {
+  const offset = coefficients.reduce(
+    (acc, curr, i) => acc + curr * Math.pow(itemCount, coefficients.length - 1 - i),
     0,
   );
-  return parseFloat(y.toFixed(2));
+
+  return parseFloat(offset.toFixed(2));
 };
